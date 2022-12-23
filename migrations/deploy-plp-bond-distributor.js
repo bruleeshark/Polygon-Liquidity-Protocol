cf39c4d735f06cedfc5c276e9806ff447c2be10b
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2022-12-23 15:36:54 -0700

    initial commit

---
+++ migrations/deploy-plp-bond-distributor.js
@@ -0,0 +1,28 @@
+// ./projectfolder/scripts/deploy-plp-bond-distributor.js
+
+// Import the Hardhat runtime environment
+const { use, task } = require("hardhat");
+
+// Import the `deploy` function from the `hardhat-deploy` plugin
+const { deploy } = require("hardhat-deploy");
+
+// Import the contract artifacts
+const PLPBondDistributor = require("../build/PLPBondDistributor.json");
+
+// Configure the task to deploy the PLPBondDistributor contract
+task("deploy-plp-bond-distributor", "Deploy the PLP Bond Distributor contract")
+  .addParam("multisig", "The address of the multisig contract")
+  .addParam("treasury", "The address of the treasury contract")
+  .setAction(async ({ multisig, treasury }) => {
+    // Deploy the PLPBondDistributor contract
+    const contract = await deploy(PLPBondDistributor, [multisig, treasury]);
+
+    // Print the deployed contract address
+    console.log(`PLP Bond Distributor contract deployed at: ${contract.address}`);
+  });
+
+// Use the Hardhat NomicLabs fork of Ethereum
+use("@nomiclabs/hardhat-ethereum-fork");
+
+// Use the mainnet network configuration
+use("hardhat-network-mainnet");
