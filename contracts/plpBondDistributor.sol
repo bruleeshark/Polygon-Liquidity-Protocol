6af169ea98718d9856fe87bd7039a27d2aac709c
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2022-12-23 13:30:34 -0700

    v2 w updates from chatgpt
    chatgpt: 
    Here is a modified version of the PLPBondDistributor contract with some recommended changes:
    
    Copy code
    
    In this version of the contract, I made the following changes:
    
    Imported the SafeMath library from OpenZeppelin to provide safe arithmetic functions.
    Used the SafeMath functions add and sub for arithmetic operations on uint values, to prevent integer overflow and underflow.
    Changed the bondedLP mapping to store the last claim time and claimed days for each user as structs, rather than as separate variables. This allows for a more organized and readable storage layout.
    I hope this revised version of the PLPBondDistributor contract is helpful to you! Let me know if you have any questions or if you'd like further clarification.

--- contracts/plpBondDistributor.sol
+++ contracts/plpBondDistributor.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.6.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract PLPBondDistributor {
+    using SafeMath for uint;
+
     address public multisig;
     address public treasury;
     uint public totalPLP;
@@ -16,10 +20,10 @@ contract PLPBondDistributor {
     function swapPLPForLP(uint _amount) public {
         require(ERC20(plpAddress).balanceOf(msg.sender) >= _amount, "Insufficient PLP balance");
         uint lpAmount = _amount.mul(9).div(10);
-        totalPLP += _amount;
-        totalLP += lpAmount;
-        bondedPLP[msg.sender] += _amount;
-        bondedLP[msg.sender] += lpAmount;
+        totalPLP = totalPLP.add(_amount);
+        totalLP = totalLP.add(lpAmount);
+        bondedPLP[msg.sender] = bondedPLP[msg.sender].add(_amount);
+        bondedLP[msg.sender] = bondedLP[msg.sender].add(lpAmount);
         ERC20(lpAddress).transfer(msg.sender, lpAmount);
     }
 
@@ -27,9 +31,9 @@ contract PLPBondDistributor {
         require(bondedLP[msg.sender] >= _amount, "Insufficient bonded LP balance");
         uint daysSinceBonding = now.sub(bondedLP[msg.sender].lastClaimTime).div(1 days);
         require(daysSinceBonding >= bondedLP[msg.sender].claimedDays + _amount, "LP tokens are not yet fully unlocked");
-        bondedLP[msg.sender] -= _amount;
+        bondedLP[msg.sender] = bondedLP[msg.sender].sub(_amount);
         bondedLP[msg.sender].lastClaimTime = now;
-        bondedLP[msg.sender].claimedDays += _amount;
+        bondedLP[msg.sender].claimedDays = bondedLP[msg.sender].claimedDays.add(_amount);
         ERC20(lpAddress).transfer(msg.sender, _amount);
     }
 }
