7b9691e63a76852edb836887e8d3b0b758dcd2c2
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2022-12-23 17:54:19 -0700

    update for Oracle compatibility

--- contracts/plpERC20.sol
+++ contracts/plpERC20.sol
@@ -7,9 +7,19 @@ contract plpERC20 is SafeERC20 {
     string public symbol = "PLP";
     uint8 public decimals = 18;
     uint public totalSupply = 7000000 * (10 ** uint(decimals));
+    PriceFeed public priceFeed;
+    PriceOracle public priceOracle;
 
     constructor() public {
         // mint all tokens and assign them to the contract address
         _mint(address(this), totalSupply);
+        // Set the address of the oracle contract as the price feed
+        priceFeed = PriceOracle(priceOracle);
     }
+}
+
+  // Function to set the address of the oracle contract
+  function setPriceOracle(PriceOracle _priceOracle) public onlyOwner {
+    priceOracle = _priceOracle;
+  }
 }
