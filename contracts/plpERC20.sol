4b7f9b44195062489e1ca86c538d32b3e8bbc304
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2022-12-23 17:57:38 -0700

    update v 0.03

--- contracts/plpERC20.sol
+++ contracts/plpERC20.sol
@@ -1,14 +1,18 @@
 pragma solidity ^0.6.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "./PriceFeed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/Address.sol";
 
 contract plpERC20 is SafeERC20 {
+    using SafeMath for uint;
     string public name = "Polygon Liquidity Protocol";
     string public symbol = "PLP";
     uint8 public decimals = 18;
     uint public totalSupply = 7000000 * (10 ** uint(decimals));
     PriceFeed public priceFeed;
-    PriceOracle public priceOracle;
+    PriceOracle private priceOracle;
 
     constructor() public {
         // mint all tokens and assign them to the contract address
@@ -16,10 +20,17 @@ contract plpERC20 is SafeERC20 {
         // Set the address of the oracle contract as the price feed
         priceFeed = PriceOracle(priceOracle);
     }
-}
 
-  // Function to set the address of the oracle contract
-  function setPriceOracle(PriceOracle _priceOracle) public onlyOwner {
-    priceOracle = _priceOracle;
-  }
+    function setPriceOracle(PriceOracle _priceOracle) public onlyOwner {
+        require(Address.isContract(_priceOracle), "Provided address is not a contract");
+        priceOracle = _priceOracle;
+        priceFeed = PriceOracle(priceOracle);
+        emit OracleUpdated(priceOracle);
+    }
+
+    function getPrice() public view returns (uint) {
+        return priceFeed.latestAnswer();
+    }
+
+    event OracleUpdated(PriceOracle _priceOracle);
 }
