35d6b8d3a3ef294b7a855084e15be053c1cc9355
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2022-12-23 18:03:56 -0700

    updated version 0.02

--- contracts/plpOracle.sol
+++ contracts/plpOracle.sol
@@ -4,8 +4,10 @@ import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/Saf
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/Address.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./IBalancerPool.sol";
 
-contract plpOracle {
+contract plpOracle is Ownable {
     using SafeMath for uint256;
     using Address for address;
 
@@ -16,11 +18,14 @@ contract plpOracle {
     constructor(address _balancerPool) public {
         owner = msg.sender;
         balancerPool = _balancerPool;
+        require(Address(balancerPool).isContract(), "Balancer pool address is not a contract");
     }
 
-    function updatePrice() onlyOwner {
+    function updatePrice() public onlyOwner {
+        require(Address(balancerPool).isContract(), "Balancer pool address is not a contract");
         // Fetch the current price of the PLP token in the Balancer pool
         price = IBalancerPool(balancerPool).getTokenPrice(address(this)).div(1e18);
+        emit PriceUpdated(price);
     }
 
     function getPrice() public view returns (uint256) {
